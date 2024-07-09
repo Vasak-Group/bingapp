768dffb13a19a69cfdd50d9315ab8b6759a092a8
Joaquin (Pato) Decima <jdecima@bancodelsol.com>
2024-07-09 19:01:50 -0300

    chore: Update landing page with TailwindCSS styling

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div
+    class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4"
+  >
+    <div class="flex-shrink-0">
+      <img
+        class="h-12 w-12"
+        src="https://static-00.iconduck.com/assets.00/chat-icon-2048x2048-i7er18st.png"
+        alt="ChitChat Logo"
+      />
+    </div>
+    <div>
+      <div class="text-xl font-medium text-black">Vasak Tailwind Test</div>
+      <p class="text-gray-500">Ya esta tailwind funcionado!</p>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
